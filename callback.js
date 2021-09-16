09d46dcdca102b0d818592032b223c3ad569ef88
Venezia Benjamin <benjamin.venezia@golem.ai>
2021-09-16 18:12:36 +0200

    callback api with url link

---
+++ callback.js
@@ -0,0 +1,31 @@
+const getTodos = (callback) => {
+
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange', () => {
+        
+        if (request.readyState === 4 && request.status === 200) {
+            const data = JSON.parse(request.responseText);
+            callback(undefined, data);
+        } else if(request.readyState === 4) {
+            callback('could not fetch data', undefined);
+        }
+    })
+    
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+    request.send();
+};
+
+getTodos((err, data) => { //Il y a une fonction en paramètre ici
+    console.log('callback fired')
+  
+    if(err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
+
+
+
+ 
