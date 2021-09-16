5d354887b1441ec8e15e92f926cf7f9b26c4f6c9
Venezia Benjamin <benjamin.venezia@golem.ai>
2021-09-16 21:43:32 +0200

    end promises exemple with promises chaining.

--- callback.js
+++ callback.js
@@ -1,31 +1,60 @@
-const getTodos = (callback) => {
 
-    const request = new XMLHttpRequest();
+const getTodos = (resource) => {
 
-    request.addEventListener('readystatechange', () => {
-        
-        if (request.readyState === 4 && request.status === 200) {
-            const data = JSON.parse(request.responseText);
-            callback(undefined, data);
-        } else if(request.readyState === 4) {
-            callback('could not fetch data', undefined);
-        }
-    })
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
     
-    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-    request.send();
+        request.addEventListener('readystatechange', () => {
+            
+            if (request.readyState === 4 && request.status === 200) {
+                const data = JSON.parse(request.responseText);
+                resolve(data);
+            } else if(request.readyState === 4) {
+                reject('error getting resource');
+            }
+        });
+        
+        request.open('GET', resource);
+        request.send();
+    });
 };
 
-getTodos((err, data) => { //Il y a une fonction en paramètre ici
-    console.log('callback fired')
-  
-    if(err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
+getTodos('todos/luigi.json').then(data => {
+    console.log('promise resolved', data);
+    return getTodos('todos/mario.json');
+}).then(data => {
+    console.log('promise 2 resolved', data);
+    return getTodos('todos/benjamin.json');
+}).then(data => {
+    console.log('promise 3 resolved', data);
+}).catch(err => {
+    console.log('promise rejected', err);
 });
 
 
 
+/************** Example of promise  *************/
+//promise example
+// const getSomething = () => {
+//     return new Promise((resolve, reject) => {
+//         //fetch something
+//         resolve('some data'); //resolve something or...
+//         reject('some error'); //reject something
+//     });
+
+// };
+
+// getSomething().then((data) => { //first function (resolve)
+//     console.log(data);
+// }, (err) => {                      //second function (reject)
+//     console.log(err);
+// });
+
+//another way to write --^
+//better because is easiest to do chaining promises
+// getSomething().then(data => {
+//     console.log(data);
+// }).catch(err => { //reject
+//     console.log(err);
+// }) 
  
