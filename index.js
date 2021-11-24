236eaacb006518caf58fcfb573f3e25fec78de3c
Venezia Benjamin <benjamin.venezia@golem.ai>
2021-11-24 11:43:42 +0100

    initialize pie chart project

--- index.js
+++ index.js
@@ -3,13 +3,7 @@ class App extends React.Component
     render() {
         return (
             <div>
-                <Hello 
-                    from="Ben" 
-                    to={"Noemi"} 
-                    bangs={2}
-                    data={[22, 33, 44, 55]}
-                    
-                />
+                <PieChart />
             </div>
         );
     }
